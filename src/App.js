b9a6ccdf2282eb510f64d748010820850c50bb22
joelsonrodrigues <rodriguesjoelson002@gmail.com>
2022-07-16 12:10:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BsFacebook,BsLinkedin,BsGithub} from "react-icons/bs"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App(){
+  return(
+    <div>
+        <div className="container">
+          <div className="icones">
+            <button><BsFacebook className="face"/></button>
+            <button ><BsLinkedin className="linkdin"/></button>
+            <button ><BsGithub className="github"/></button>
+          </div>
+        </div> 
+                
     </div>
   );
 }
-
-export default App;
